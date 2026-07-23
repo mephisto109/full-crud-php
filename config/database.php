42c0cea82b984ddc959ceef1f6e4e7d6239bd6fb
mephisto109 <rahmakhoyrulhawa@gmail.com>
2026-07-23 08:16:50 +0700

    ganti usn

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect("localhost", "rahmakhoyrul-crud-php", "rahmacrudphp", "rahmakhoyrul_git rud_php_");
+$db = mysqli_connect("localhost", "rahmakhoyrul-crud-php", "rahmacrudphp", "rahmakhoyrul_crud_php_db");
 //if (!$db) {
   //  die("Gagal terhubung ke database: " . mysqli_connect_error());
 //} else {
